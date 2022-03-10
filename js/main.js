6f3ea65a9e551df1f6578cb7b7034641144b6572
alexeiromanov41 <alexeiromanov41@gmail.com>
2022-03-10 14:41:29 +0300

    calc 2_1

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ calcWireD.addEventListener('click', () => {
 	let S = sqIn.value;
 	if (S <= 0) {
 		alert('Недопустимое значение');
+	} else {
+		let D = Math.sqrt((4 * S) / Math.PI).toFixed(2);
+		console.log(D);
+		dOut.value = D;
 	}
-	let D = Math.sqrt((4 * S) / Math.PI).toFixed(2);
-	console.log(D);
-	dOut.value = D;
 });
 
 // console.log(S);
