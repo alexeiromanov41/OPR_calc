39deab01295da1d588bf031b876dbb18b089ca13
alexeiromanov41 <alexeiromanov41@gmail.com>
2022-03-10 16:16:40 +0300

    calc 2 ready wtht kef v1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/***** Вычисление диаметра провода по сечению      begin*************/
 const calcWireD = document.getElementById('calcWireD');
 const sqIn = document.getElementById('wireS');
 const dOut = document.getElementById('wireD');
@@ -12,5 +13,44 @@ calcWireD.addEventListener('click', () => {
 		dOut.value = D;
 	}
 });
+/***** Вычисление диаметра провода по сечению      end*************/
 
-// console.log(S);
+/***** Расчет емкости АКБ      begin*************/
+const u1AKB = document.getElementById('u1AKB');
+const nAKBG = document.getElementById('nAKBG');
+const nGroup = document.getElementById('nGroup');
+const powerD = document.getElementById('powerD');
+const timeD = document.getElementById('timeD');
+const kpdUPS = document.getElementById('kpdUPS');
+const CAKB = document.getElementById('CAKB');
+const calcCAKB = document.getElementById('calcCAKB');
+
+calcCAKB.addEventListener('click', () => {
+	let u1 = u1AKB.value;
+	let n = nAKBG.value;
+	let nG = nGroup.value;
+	let power = powerD.value;
+	let t = timeD.value;
+	let kpd = kpdUPS.value;
+	let C;
+	let uAll;
+	let cellNumber;
+	let dischargeConstPower;
+	let dischargeConstCurrent;
+	let kEf = 0.7;
+	let cEf;
+
+	if (u1 < 2 || n < 1 || nG < 1 || power < 1 || t < 1 || kpd < 0.85) {
+		alert('Недопустимое значение');
+	} else {
+		uAll = u1 * n;
+		cellNumber = u1 / 2;
+		dischargeConstPower = power / n / cellNumber / nG;
+		dischargeConstCurrent = dischargeConstPower / 1.9;
+		cEf = (t * dischargeConstCurrent) / 60;
+		C = (cEf / kEf / kpd).toFixed(2);
+
+		CAKB.value = C;
+	}
+});
+/***** Расчет емкости АКБ      end*************/
