a98704133e8bdb3f21e992b0a275e7d200c8f12d
alexeiromanov41 <alexeiromanov41@gmail.com>
2022-03-11 16:49:59 +0300

    calc koef

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ const timeD = document.getElementById('timeD');
 const kpdUPS = document.getElementById('kpdUPS');
 const CAKB = document.getElementById('CAKB');
 const calcCAKB = document.getElementById('calcCAKB');
+let cEf;
 
 calcCAKB.addEventListener('click', () => {
 	let u1 = u1AKB.value;
@@ -37,10 +38,104 @@ calcCAKB.addEventListener('click', () => {
 	let cellNumber;
 	let dischargeConstPower;
 	let dischargeConstCurrent;
-	let kEf = 0.7;
 	let cEf;
 
-	if (u1 < 2 || n < 1 || nG < 1 || power < 1 || t < 1 || kpd < 0.85) {
+	const getK = () => {
+		let kEfCalc;
+		const tableK = [
+			{
+				timeK: 5,
+				koef: 0.18,
+			},
+			{
+				timeK: 10,
+				koef: 0.4,
+			},
+			{
+				timeK: 15,
+				koef: 0.5,
+			},
+			{
+				timeK: 20,
+				koef: 0.5,
+			},
+			{
+				timeK: 25,
+				koef: 0.58,
+			},
+			{
+				timeK: 30,
+				koef: 0.61,
+			},
+			{
+				timeK: 35,
+				koef: 0.64,
+			},
+			{
+				timeK: 40,
+				koef: 0.67,
+			},
+			{
+				timeK: 45,
+				koef: 0.68,
+			},
+			{
+				timeK: 50,
+				koef: 0.7,
+			},
+			{
+				timeK: 55,
+				koef: 0.72,
+			},
+			{
+				timeK: 60,
+				koef: 0.73,
+			},
+			{
+				timeK: 90,
+				koef: 0.8,
+			},
+			{
+				timeK: 120,
+				koef: 0.84,
+			},
+			{
+				timeK: 600,
+				koef: 1,
+			},
+		];
+
+		for (let index = 0; index < tableK.length; index++) {
+			const element = tableK[index];
+			if (element.timeK == t) {
+				kEfCalc = element.koef;
+				break;
+			} else if (element.timeK > t) {
+				kEfCalc = 0.01;
+				break;
+			} else {
+				kEfCalc = 1;
+				break;
+			}
+		}
+
+		// tableK.forEach((element, index) => {
+		// 	if (element.timeK == t) {
+		// 		console.log(index);
+		// 		return (kEfCalc = element.koef);
+		// 	} else if (element.timeK > t) {
+		// 		return (kEfCalc = 0.01);
+		// 	} else {
+		// 		return (kEfCalc = 1);
+		// 	}
+		// });
+		return kEfCalc;
+	};
+
+	let kEf = getK();
+	console.log(kEf);
+
+	if (u1 < 2 || n < 1 || nG < 1 || power < 1 || t < 5 || kpd < 0.85) {
 		alert('Недопустимое значение');
 	} else {
 		uAll = u1 * n;
